9e7af43e787bb93e5688f8b22cad106ac35b14fb
criti0728 <criti0728@gmail.com>
2024-08-25 13:10:59 +0900

    [PGS]/DFS/shpark

---
+++ seohapark/Week07/pgs_network/Solution_dfs.java
@@ -0,0 +1,28 @@
+import java.util.*;
+//문제가 타겟넘버와 거의 똑같으니 주석은 대충 달겠음(딮스 빞스 둘다)
+class Solution_dfs {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n]; //너 익숙한 얼굴이니?
+
+        for (int com = 0; com < n; com++) {
+             if (!visited[com]) { // 아직 방문하지 않은 컴퓨터가 있다면
+                dfs(n, computers, com, visited);
+                 answer++; // 넷웍 하나 더
+            }
+        }
+
+        return answer;
+    }
+
+    // 국룰 메서드
+    private void dfs(int n, int[][] computers, int com, boolean[] visited) {
+        visited[com] = true; // 콤에 들어간 메서드는 이미 방문한거다? 메모
+
+        for (int i = 0; i < n; i++) { //스캔
+            if (i != com && computers[com][i] == 1 && !visited[i]) {//커런트가 아닌 방문안한 노드가 있고 연결되지 않은 다른 컴퓨타가 있다면(연결대상이라면)
+                dfs(n, computers, i, visited); // 자 다시 갔다와
+            }
+        }
+    }
+}
---
+++ seohapark/Week07/pgs_targetnum/Solution_dfs.java
@@ -0,0 +1,24 @@
+class Solution_dfs { 
+    public int solution(int[] numbers, int target) { 
+        return dfs(numbers, target, 0, 0);  // dfs 메서드 불러와줘 초기값 0에 합도 0으로 시작할게
+    }
+
+    private int dfs(int[] numbers, int target, int depth, int sum) {  // 재귀 메서드
+        if (depth == numbers.length) {  // 다 끝난 상태에서
+            if (sum == target) {  //지금ㄲ싸지의 합이 목표치와 동일하다면
+                return 1;  // 이 메서드 결과값은 1이야
+            } else {  // 아님
+                return 0;  // 말고(짜피 못만드니 gg)
+            }
+        } else {  // 뒤져볼게 더 있으면
+            int count = 0;  // 일단 카운트값 리셋
+            count += dfs(numbers, target, depth + 1, sum + numbers[depth]);  //지금 숫자 더해서 탐색, 결과값을 카운트로
+         count += dfs(numbers, target, depth + 1, sum - numbers[depth]);  //지금 숫자 빼서 탐색, 결과값을 카운트로
+            return count;  // 이게 우리가 원하는 값
+        }
+    }
+}
+/*
+ * 지금까지의 합과 깊이를 받아와서 모든 경우를 탐색하고 마지막 인자까지 탐색
+ * 메모리 사용량은 의외로 적을 수 있으나 스택의 깊이가 너무 깊어질지도...
+ */
